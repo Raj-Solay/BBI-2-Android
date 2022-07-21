c3ed6456d34b6832d66540de926f01462913a546
yugal <yugal.yug123@gmail.com>
2022-07-21 23:31:07 +0530

    date formate   ui 20/07/2022

--- app/src/main/java/com/bbi/bizbulls/utils/Globals.kt
+++ app/src/main/java/com/bbi/bizbulls/utils/Globals.kt
@@ -1,6 +1,12 @@
 package com.bbi.bizbulls.utils
 
+import android.os.Build
 import com.bbi.bizbulls.enums.Environment
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.ZoneOffset.UTC
+import java.util.*
 
 /**
  * Singleton container for application constants and globals
@@ -44,5 +50,16 @@ object Globals {
     const val USER_TYPE_EMPLOYEE = 2
     const val USER_TYPE_FO_TEAM = 3
     const val USER_TYPE_FO = 1
+    //const val FORMATEZTIME = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+    fun dateFormat(dete:String): String?{
 
+        var dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
+
+        var objDate = dateFormat.parse(dete);
+
+        var dateFormat2 = SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        var finalDate = dateFormat2.format(objDate)
+       return finalDate
+    }
 }
